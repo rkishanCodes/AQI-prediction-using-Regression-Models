ddcc6459e77bac4ea98e7f6f3201fea13ebb7127
rkishanCodes <rkishan.codes@gmail.com>
2024-09-20 22:42:58 +0530

    added os path

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
+import os
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
 
 app = Flask(__name__)
 
+# Define the base directory for models
+base_dir = os.path.dirname(os.path.abspath(__file__))
+models_dir = os.path.join(base_dir, 'models')
+
 model_paths = {
-     "Decision Tree": "models/Decision_tree_model.pkl",
-    "Random Forest": "models/random_forest_model.pkl",
-    "KNN": "models/KNN_model.pkl",
-    "Linear Regression": "models/linear_regression_model.pkl",
-    "Gradient Boost": "models/Gradient_boost_model.pkl",
+    "Decision Tree": os.path.join(models_dir, "Decision_tree_model.pkl"),
+    "Random Forest": os.path.join(models_dir, "random_forest_model.pkl"),
+    "KNN": os.path.join(models_dir, "KNN_model.pkl"),
+    "Linear Regression": os.path.join(models_dir, "linear_regression_model.pkl"),
+    "Gradient Boost": os.path.join(models_dir, "Gradient_boost_model.pkl"),
 }
 
 models = {}
@@ -22,11 +27,6 @@ def index():
     return render_template('index.html')
 
 @app.route('/predict_aqi', methods=['POST'])
-
-
-
-
-
 def predict_aqi():
     model_name = request.form['model']
     pm25 = float(request.form['pm25'])
@@ -36,9 +36,8 @@ def predict_aqi():
     so2 = float(request.form['so2'])
     co = float(request.form['co'])
     ozone = float(request.form['ozone'])
+
     model = models[model_name]
-
-
     input_data = np.array([[pm25, pm10, no2, nh3, so2, co, ozone]])
     predicted_aqi = model.predict(input_data)
 
@@ -46,5 +45,3 @@ def predict_aqi():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
